3e06b13b7fa86aa4809953caf8a2b739b5aaf695
JOB0811 <34418169+JOB0811@users.noreply.github.com>
2024-10-02 21:37:39 +0900

    UI연결중

--- Assets/4.Scripts/Player/Interaction/PlayerStat.cs
+++ Assets/4.Scripts/Player/Interaction/PlayerStat.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
 public class PlayerStat : MonoBehaviourPun
 {
     public float maxHealth = 100f;
     private float currentHealth;
 
+    public Slider healthSlider;
+    public Text healthCount;
+
+
     private void Start()
-    {
+    {        
+
+        healthSlider.maxValue = maxHealth;
         currentHealth = maxHealth;
+
     }
 
     //플레이어가 공격받았을 때,
@@ -22,6 +30,10 @@ public class PlayerStat : MonoBehaviourPun
 
         currentHealth -= damage;
         Debug.Log($"플레이어가 {damage}의 데미지를 입었습니다. 남은 체력 : {currentHealth}");
+
+        healthSlider.value = currentHealth/100;
+        healthCount.text = (currentHealth/100).ToString();
+
         if (currentHealth <= 0)
         { Die(); }
     }
